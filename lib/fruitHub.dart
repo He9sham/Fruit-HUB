3b893420dbebdc58176fdc04365d2672eb943e6f
hesha30m <95132216+He9sham@users.noreply.github.com>
2024-09-23 02:37:10 +0300

    add flavors and finsh logic him <3

---
+++ lib/fruitHub.dart
@@ -0,0 +1,14 @@
+
+import 'package:commerce_hub/feature/Onboarding&Splash/view/splash_view.dart';
+import 'package:flutter/material.dart';
+
+class CommerceHub extends StatelessWidget {
+  const CommerceHub({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: SplashView(),
+    );
+  }
+}
