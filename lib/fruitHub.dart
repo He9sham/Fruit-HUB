151d3d362535393f292634538ec6a35eef6e96ae
hesha30m <95132216+He9sham@users.noreply.github.com>
2024-09-23 03:11:01 +0300

    create splash screen

--- lib/fruitHub.dart
+++ lib/fruitHub.dart
@@ -1,14 +1,18 @@
-
 import 'package:commerce_hub/feature/Onboarding&Splash/view/splash_view.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class CommerceHub extends StatelessWidget {
   const CommerceHub({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: SplashView(),
+    return const ScreenUtilInit(
+      designSize: Size(375, 812),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: SplashView(),
+      ),
     );
   }
 }
--- lib/main_Production.dart
+++ lib/main_Production.dart
@@ -1,11 +1,8 @@
-import 'package:commerce_hub/core/notification_service/local_notification_service.dart';
 import 'package:commerce_hub/fruitHub.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 void main() async {
-  NotificationService services = NotificationService();
-  await services.init();
   await ScreenUtil.ensureScreenSize();
   runApp(
     const CommerceHub(),
