25520856d93befaf9182488d3ec8fa88588194ef
hesha30m <95132216+He9sham@users.noreply.github.com>
2024-10-25 18:45:25 +0300

    finsh app bar for home view

--- lib/feature/home/view/home_view.dart
+++ lib/feature/home/view/home_view.dart
@@ -1,3 +1,5 @@
+import 'package:commerce_hub/core/helper/spacing.dart';
+import 'package:commerce_hub/feature/home/view/widgets/custom_appbar_home_view.dart';
 import 'package:flutter/material.dart';
 
 class HomeView extends StatelessWidget {
@@ -5,13 +7,17 @@ class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('HomeView'),
-      ),
-      body: Column(
-        children: [
-          Container(),
-        ],
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: SafeArea(
+          child: Column(
+            children: [
+              verticalSpace(16),
+              const CustomAppbarHomeview(),
+              verticalSpace(25),
+            ],
+          ),
+        ),
       ),
     );
   }
---
+++ lib/feature/home/view/widgets/custom_appbar_home_view.dart
@@ -0,0 +1,43 @@
+import 'package:commerce_hub/core/helper/spacing.dart';
+import 'package:commerce_hub/core/theming/styles.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class CustomAppbarHomeview extends StatelessWidget {
+  const CustomAppbarHomeview({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      textDirection: TextDirection.rtl,
+      children: [
+        Image.asset('assets/image/Mask group.png'),
+        horizontalSpace(11),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              '..!صباح الخير',
+              style: Styles.textRowNavigate16gray,
+            ),
+            Text(
+              'هشام حمدان',
+              style: Styles.textSize13Black600
+                  .copyWith(fontWeight: FontWeight.w700, fontSize: 16),
+            ),
+          ],
+        ),
+        horizontalSpace(155),
+        Container(
+          width: 50,
+          height: 50,
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            color: Colors.green.withOpacity(0.4),
+          ),
+          child: const Icon(FontAwesomeIcons.bell),
+        )
+      ],
+    );
+  }
+}
