664df3bcc57655747a1fc46ea1fd9da27594d799
DESKTOP-B5HI9JS\hp <heshamhamdan51@gmail.com>
2025-02-12 03:54:15 +0200

    feat: add draggable bottom sheet for sorting options in RowTextandIcon widget; implement RowTextChosse for display stiil work this feature

--- lib/feature/home/view/widgets/row_text.dart
+++ lib/feature/home/view/widgets/row_text.dart
@@ -1,3 +1,6 @@
+import 'dart:ui';
+
+import 'package:commerce_hub/core/helper/spacing.dart';
 import 'package:flutter/material.dart';
 
 import '../../../../core/theming/styles.dart';
@@ -16,12 +19,118 @@ class RowTextandIcon extends StatelessWidget {
         ),
         const Spacer(),
         IconButton(
-          onPressed: () {},
+          onPressed: () {
+            showDargSheet(context);
+          },
           icon: const Icon(
             Icons.filter_alt_outlined,
             color: Color(0xff949D9E),
           ),
-        )
+        ),
+      ],
+    );
+  }
+
+  void showDargSheet(BuildContext context) {
+    BackdropFilter(
+      filter: ImageFilter.blur(sigmaX: 50, sigmaY: 50),
+      child: Container(
+        color: Colors.black.withValues(alpha: 0.4),
+      ),
+    );
+    showModalBottomSheet(
+      backgroundColor: Colors.transparent,
+      context: context,
+      builder: (context) {
+        return DraggableScrollableSheet(
+          initialChildSize: 0.60,
+          minChildSize: 0.3,
+          maxChildSize: 0.6,
+          builder: (context, scrollController) {
+            return Container(
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(5),
+                  topRight: Radius.circular(5),
+                ),
+              ),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  verticalSpace(15),
+                  Divider(
+                    thickness: 3,
+                    indent: 150,
+                    endIndent: 150,
+                    color: Colors.black,
+                  ),
+                  verticalSpace(25),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 170),
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 16),
+                      child: Column(
+                        children: [
+                          Text(
+                            '       : ترتيب حسب',
+                            style: Styles.textappBar.copyWith(
+                              fontWeight: FontWeight.w800,
+                            ),
+                          ),
+                          verticalSpace(15),
+                          RowTextChosse(
+                            text: 'الأعلى سعرًا',
+                          ),
+                        
+                          RowTextChosse(
+                            text: 'الأقل سعرًا',
+                          ),
+                          
+                          RowTextChosse(
+                            text: 'الأبجديه',
+                          ),
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            );
+          },
+        );
+      },
+    );
+  }
+}
+
+class RowTextChosse extends StatelessWidget {
+  const RowTextChosse({super.key, required this.text});
+  final String text;
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Text(
+          text,
+          style: Styles.textbuttom16White.copyWith(
+            color: Colors.black,
+            fontSize: 13,
+          ),
+        ),
+        horizontalSpace(15),
+        Container(
+          width: 20,
+          height: 20,
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            border: Border.all(
+                color: Colors.black.withValues(
+                  alpha: 0.3,
+                ),
+                width: 2),
+          ),
+        ),
       ],
     );
   }
