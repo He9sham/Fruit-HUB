5a7d62d92431ba7bff1d8a06ee0c29e85a43a79b
DESKTOP-B5HI9JS\hp <heshamhamdan51@gmail.com>
2025-05-14 01:59:22 +0300

    refactor: enhance product view with new search method and improve UI responsiveness add : Item details view

---
+++ lib/feature/Item_details/view/item_details.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ItemDetails extends StatelessWidget {
+  const ItemDetails({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Item Details'),
+      ),
+      body: const Center(
+        child: Text('Item Details'),
+      ),
+    );
+  }
+}
